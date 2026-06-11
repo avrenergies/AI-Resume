6f48fa44fee313e92d5d0e76b87d3f4d409b5395
rani.b <ranibalan93@gmail.com>
2026-06-11 11:47:13 +0530

    "Latest avr resume  extraction"

--- app/job_title_provider.py
+++ app/job_title_provider.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 
 API_URL = "https://api.avrenergies.com/job-posting/departments/with-job-titles"
@@ -21,28 +20,4 @@ def fetch_job_titles_from_api():
         return titles
 
     except Exception:
-=======
-import requests
-
-API_URL = "https://api.avrenergies.com/job-posting/departments/with-job-titles"
-
-def fetch_job_titles_from_api():
-    try:
-        res = requests.get(API_URL, timeout=5)
-
-        if res.status_code != 200:
-            return []
-
-        data = res.json()
-
-        titles = []
-        for dept in data.get("data", []):
-            for job in dept.get("jobTitles", []):
-                if job.get("name"):
-                    titles.append(job["name"].strip())
-
-        return titles
-
-    except Exception:
->>>>>>> 9d2d7face242eebb6a4a3d878a35ead4285cf42d
         return []
